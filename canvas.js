cb18133546db245357aeaa5972bce7ef3bcf6261
jwong65 <jason.wong6507@gmail.com>
2023-11-23 16:51:00 -0500

    Added all the ctx draw inside the animationLoop and added the clear canvas.

--- canvas.js
+++ canvas.js
@@ -4,8 +4,6 @@ const ctx = canvas.getContext('2d')
 canvas.width = 64*16 
 canvas.height = 64*9
 
-// fillRect( x value, y value, width, height)
-ctx.fillRect(0,0, canvas.width, canvas.height)
 
 let charY = 100
 let charX = 100
@@ -16,6 +14,14 @@ ctx.fillRect(charX, charY, 100,100)
 function animationLoop(){
     // Callback to the animation keep animating.
     window.requestAnimationFrame(animationLoop)
+    // fillRect( x value, y value, width, height) This is the background of the whole canvas.
+    ctx.fillStyle='black'
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle='#FF8F8F'
+    ctx.fillRect( charX, charY, 100, 100)
+    charY++
 }
 
 animationLoop()
