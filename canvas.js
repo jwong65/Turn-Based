bed9d510f937e4753f3b4242c1d20235e6ee392a
jwong65 <jason.wong6507@gmail.com>
2023-11-23 16:45:39 -0500

    Added the variables for the character X and Y values. Changed it for the inital drawing of the character

--- canvas.js
+++ canvas.js
@@ -7,6 +7,15 @@ canvas.height = 64*9
 // fillRect( x value, y value, width, height)
 ctx.fillRect(0,0, canvas.width, canvas.height)
 
+let charY = 100
+let charX = 100
 ctx.fillStyle='#FF8F8F'
 // x, y, width, height
-ctx.fillRect(100, 100, 100,100)
+ctx.fillRect(charX, charY, 100,100)
+
+function animationLoop(){
+    // Callback to the animation keep animating.
+    window.requestAnimationFrame(animationLoop)
+}
+
+animationLoop()
