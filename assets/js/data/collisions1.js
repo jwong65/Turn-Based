a85cd96c1276165ebfc0a75359ccc6192652788c
jwong65 <jason.wong6507@gmail.com>
2023-11-24 03:43:22 -0500

    Used a prototype to help visualize the collision detection

--- assets/js/data/collisions1.js
+++ assets/js/data/collisions1.js
@@ -16,3 +16,17 @@ const collisionsLevel1 = [225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225,
     225, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 225,
     225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225,
     225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225, 225]
+
+    // This is to help visualize it like the tutorial tried.
+Array.prototype.parse2D=function(){
+    const rows=[]
+    // Increment by 32 because that is the width of this map 32*18 tiles.
+    for (let i=0; i< this.length; i+=32){
+        // This will grab the first 32 tiles and the first row.
+        rows.push(this.slice(i, i+32))
+    }
+return rows
+}
+
+const parsedCollisions = collisionsLevel1.parse2D()
+console.log(parsedCollisions)
