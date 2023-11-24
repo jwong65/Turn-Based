ec746df103ae0d2c3fce6fa43af34a1030a20043
jwong65 <jason.wong6507@gmail.com>
2023-11-24 02:09:52 -0500

    Added the eventListener into it's own seperate file and conneted it to the index.html

---
+++ assets/js/eventListener.js
@@ -0,0 +1,39 @@
+
+window.addEventListener('keydown', (event)=>{
+    // console.log(event)
+    switch(event.key){
+        case 'w':
+        case 'ArrowUp':
+            if (player.velocity.y === 0)
+            {
+                player.velocity.y = -15
+            }
+            break
+        case 'a':
+        case 'ArrowLeft':
+            keys.a.pressed =true
+            break
+        case 'd':
+        case 'ArrowRight':
+            keys.d.pressed = true
+            break
+        // case 's'
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch(event.key){
+        case 'a':
+        case 'd':
+        case 'ArrowLeft':
+        case 'ArrowRight':
+            keys.a.pressed = false
+            keys.d.pressed = false
+            break
+        case 'w':
+        case 'ArrowUp':
+            player.velocity.y = 0
+            break
+        // case 's'
+    }
+})
--- canvas.js
+++ canvas.js
@@ -21,9 +21,9 @@ function animationLoop(){
 
     player.velocity.x = 0
     if (keys.d.pressed){
-        player.velocity.x =5;
+        player.velocity.x =4;
     }else if (keys.a.pressed){
-        player.velocity.x =-5
+        player.velocity.x =-4
     }
     // This will call the draw function from the Player class.
     player.draw()
@@ -32,42 +32,3 @@ function animationLoop(){
 
 }
 animationLoop()
-
-window.addEventListener('keydown', (event)=>{
-    // console.log(event)
-    switch(event.key){
-        case 'w':
-        case 'ArrowUp':
-            if (player.velocity.y === 0)
-            {
-                player.velocity.y = -15
-            }
-            break
-        case 'a':
-        case 'ArrowLeft':
-            keys.a.pressed =true
-            break
-        case 'd':
-        case 'ArrowRight':
-            keys.d.pressed = true
-            break
-        // case 's'
-    }
-})
-
-window.addEventListener('keyup', (event)=>{
-    switch(event.key){
-        case 'a':
-        case 'd':
-        case 'ArrowLeft':
-        case 'ArrowRight':
-            keys.a.pressed = false
-            keys.d.pressed = false
-            break
-        case 'w':
-        case 'ArrowUp':
-            player.velocity.y = 0
-            break
-        // case 's'
-    }
-})
