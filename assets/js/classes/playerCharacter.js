9960b33cfd841f46cb83efa07ebb08481d792e56
jwong65 <jason.wong6507@gmail.com>
2023-11-23 20:28:37 -0500

    Added the value of gravity instead of incrementing by 1, in case this needs to be changed for gameplay

--- assets/js/classes/playerCharacter.js
+++ assets/js/classes/playerCharacter.js
@@ -5,22 +5,32 @@ class Player{
             charX: 100,
             charY: 100
         }
+        this.velocity={
+            // We need a x and y axis for each veloctiy
+            x: 0,
+            y: 0
+        }
         this.charWidth = 100,
         this.charHeight = 100
         // This is charY + height of the character. Need to have a const height so that can be applied to the fillRect.
         this.sides = {
             bottom: this.position.charY + this.charHeight
         }
+        this.gravity = 1
     }
     draw(){
         ctx.fillStyle = '#FF8F8F'
         ctx.fillRect(this.position.charX, this.position.charY, this.charWidth, this.charHeight)
     }
     update(){
+        this.position.charY += this.velocity.y
+        // Position is being increased consistently by the y velo.
         if( this.sides.bottom <canvas.height){
-            this.position.charY++
+            this.velocity.y+= this.gravity
             this.sides.bottom =this.position.charY + this.charHeight
-        }
+        } 
+        // Need this else statement otherwise it will not stop at the height of the bottom.
+        else this.velocity.y =0
 
     }
 }
